8920b4bbe7e70d641aeb3d492341687bd7d47f33
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-10-05 16:09:57 +0200

    Initial versions

---
+++ common.py
@@ -0,0 +1,3 @@
+# window size and position
+#               x    y    w    h
+WINDOW_POS = (2030, 30, 1500, 1000)
---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+#class ProdTestMonitor(QtGui.QWidget):
+class ProdTestMonitor(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(ProdTestMonitor, self).__init__()
+
+        self.setGeometry(2030, 30, 500, 200)
+        self.setWindowTitle('ActSafe Production Test')
+
+        extractAction = QtGui.QAction('&Get here', self)
+        extractAction.setShortcut('Ctrl+Q')
+        extractAction.setStatusTip('Leave the application')
+        extractAction.triggered.connect(self.close_application)
+        self.statusBar()
+
+        self.initUI()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
+    def initUI(self):
+
+        # create quit button
+        quitBtn = QtGui.QPushButton('Quit', self)
+        quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+
+        quitBtn.move(50, 50)
+        quitBtn.resize(50,40)
+
+        w = QtGui.QWidget()
+        b = QtGui.QLabel(w)
+        b.setText('Jennie')
+        b.move(100,90)
+
+    def close_application(self):
+        print('Hello')
+        choice = QtGui.QMessageBox.question(self, 'Tired? Want to go home?', 'Do you want to quit?',
+                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+
+        if choice == QtGui.QMessageBox.Yes:
+            print('GTH')
+            sys.exit()
+        else:
+            pass
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    frame = ProdTestMonitor()
+    frame.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
