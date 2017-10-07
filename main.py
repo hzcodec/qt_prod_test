19434c511123baa99cf96246a9f89f21a73ad043
heinz <heinz.samuelsson@gmail.com>
2017-10-07 18:06:46 +0200

    Rewrote main

--- main.py
+++ main.py
@@ -1,76 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import sys
 from PyQt4 import QtGui
-from PyQt4 import QtCore
-from common import Y_POS
 
-#class ProdTestMonitor(QtGui.QWidget):
-class ProdTestMonitor(QtGui.QMainWindow):
-
+class Example(QtGui.QWidget):
+    
     def __init__(self):
-        super(ProdTestMonitor, self).__init__()
-
-        self.setGeometry(2030, 30, 500, 300)
-        self.setWindowTitle('ActSafe Production Test')
-
-        extractAction = QtGui.QAction('&Quit', self)
-        extractAction.setShortcut('Ctrl+Q')
-        extractAction.triggered.connect(self.close_application)
-        self.statusBar()
-
+        super(Example, self).__init__()
+        
         self.initUI()
-
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
-        fileMenu.addAction(extractAction)
-
+        
     def initUI(self):
-
-        # bold fonts
-        myFont = QtGui.QFont('Times')
-        myFont.setBold(True)
-
-        comPortLbl = QtGui.QLabel(self)
-        comPortLbl.move(200, Y_POS)
-        comPortLbl.setText('COM port is just to long:')
-        #comPortLbl.setFont(myFont)
-
-        #comPortNameLbl = QtGui.QLabel(self)
-        #comPortNameLbl.move(280, Y_POS)
-        #comPortNameLbl.setText('ttyACM0')
-
-        #ascenderVersionLbl = QtGui.QLabel(self)
-        #ascenderVersionLbl.move(30, Y_POS+60)
-        #ascenderVersionLbl.setText('Ascender Version:')
-
-        #remoteVersionLbl = QtGui.QLabel(self)
-        #remoteVersionLbl.move(30, Y_POS+90)
-        #remoteVersionLbl.setText('Ascender Version:')
-
-        # create quit button
-        quitBtn = QtGui.QPushButton('Quit', self)
-        quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-
-        quitBtn.move(5, Y_POS)
-        quitBtn.resize(50,40)
-
-
-    def close_application(self):
-        choice = QtGui.QMessageBox.question(self, 'Tired?', 'Do you want to quit?',
-                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
-
-        if choice == QtGui.QMessageBox.Yes:
-            print('GTH')
-            sys.exit()
-        else:
-            pass
-
-
+        
+        lbl1 = QtGui.QLabel('This is a very long text hula', self)
+        lbl1.move(15, 10)
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Absolute')    
+        #self.show()
+        
 def main():
+    
     app = QtGui.QApplication(sys.argv)
-    frame = ProdTestMonitor()
+    frame = Example()
     frame.show()
     sys.exit(app.exec_())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
